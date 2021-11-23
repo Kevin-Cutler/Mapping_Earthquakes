2c73275cba46450c7d48c416f67780f04374129e
kevin-cutler <kevinecutler86@gmail.com>
2021-11-23 18:36:25 -0500

    Update Mapping Lines

--- Mapping_Multiple_Points/js/logic.js
+++ Mapping_Multiple_Points/js/logic.js
@@ -4,16 +4,16 @@ console.log("working");
 
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 
 // // Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//     40.7, -94.5
-//     ],
-//     zoom: 4
-// });
+let map = L.map("mapid", {
+    center: [
+    40.7, -94.5
+    ],
+    zoom: 4
+});
 
 
 
