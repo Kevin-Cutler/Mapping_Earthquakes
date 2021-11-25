0f97cb8762c475a2424938235e2846593f7baffb
kevin-cutler <kevinecutler86@gmail.com>
2021-11-24 19:53:41 -0500

    Updating repo

--- Mapping_GeoJSON_Points/js/logic.js
+++ Mapping_GeoJSON_Points/js/logic.js
@@ -3,7 +3,8 @@ console.log("working");
 
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 13);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
 
 
 // Add GeoJSON data.
@@ -28,22 +29,17 @@ let sanFranAirport =
 
 
 
-
-
 // Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
     // We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h2>" + sanFranAirport.city + ", " + sanFranAirport.state + "</h2> <hr> <h3>Population " + sanFranAirport.population + "</h3>");
-    
+    pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
     }
+
 }).addTo(map);
-
-
-
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
